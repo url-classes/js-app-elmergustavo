95e42b6fdc0dadc687f62a25bd2235b6ad730384
elmergustavo <elmergustavo79@gmail.com>
2022-03-07 13:39:53 -0600

    clase 07-03-2022

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+let container = 0;
+
+const cards = document.querySelectorAll(".cards");
+function cardClick(ev) {
+  const target = ev.target;
+  target.classList.add("clicked");
+  console.log(target);
+}
+
+function reset() {
+  cards.forEach((card) => card.classList.remove("clickend"));
+}
+
+cards.forEach((card) => {
+  card.addEventListener("click", cardClick);
+});
